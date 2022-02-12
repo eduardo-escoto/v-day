4eb8a0322d78d5b782a2a56a75ff8d46acf16027
Eduardo Escoto <me@eduardo.wtf>
2022-02-12 04:05:52 -0800

    overflow working

--- components/scrollCard.js
+++ components/scrollCard.js
@@ -9,50 +9,69 @@ import {
 
 import { forwardRef } from "react";
 
+// overflow y - with scrolling in card, next and bottom fixed.
 
 export default forwardRef(function ScrollCard(props, ref) {
   return (
     <div
-      style={{...props.cardStyle, minHeight: props.nextButton ? null: "100vh"}}
+      style={props.cardStyle}
       ref={ref}
-      className="border-x-2 border-pink-100 max-w-md p-12 text-xl flex flex-col text-center justify-center"
-
+      className="relative border-x-2 border-pink-100 max-w-md px-10 py-6 text-xl flex flex-col text-center justify-center max-h-screen overflow-y-auto"
       id={props.id}
     >
-      <div>
-        {props.prevButton ? (
-          <Button
-            onClick={() =>
-              props.prevRef.current.scrollIntoView({ behavior: "smooth" })
-            }
-          >
-            <FontAwesomeIcon icon={faLongArrowUp} className="text-slate-600" />
-          </Button>
-        ) : null}
-      </div>
-      <div className="max-w-sm border border-solid border-white rounded-md">
-        <Image
-          className="rounded-md"
-          src="/test.jpeg"
-          width={6}
-          height={9}
-          layout="responsive"
-          quality={100}
-          alt="pic"
-        />
-      </div>
+      <div className="min-h-full">
+        <div>
+          {props.prevButton ? (
+            <Button
+              onClick={() =>
+                props.prevRef.current.scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              <FontAwesomeIcon
+                icon={faLongArrowUp}
+                className="text-slate-600"
+              />
+            </Button>
+          ) : null}
+        </div>
+        <div className="max-w-sm border border-solid border-white rounded-md">
+          <Image
+            className="rounded-md"
+            src="/test.jpeg"
+            width={3}
+            height={4}
+            layout="responsive"
+            quality={100}
+            alt="pic"
+          />
+        </div>
+        <div className="max-w-sm border border-solid border-white rounded-md">
+          <Image
+            className="rounded-md"
+            src="/test.jpeg"
+            width={3}
+            height={4}
+            layout="responsive"
+            quality={100}
+            alt="pic"
+          />
+        </div>
         <p>{props.text}</p>
 
-      <div>
-        {props.nextButton ? (
-          <Button
-            onClick={() =>
-              props.nextRef.current.scrollIntoView({ behavior: "smooth" })
-            }
-          >
-            <FontAwesomeIcon icon={faLongArrowDown} className="text-slate-600"/>
-          </Button>
-        ) : null}
+        <div className="mb-2">
+          {props.nextButton ? (
+            <Button
+              onClick={() =>
+                props.nextRef.current.scrollIntoView({ behavior: "smooth" })
+              }
+            >
+              <FontAwesomeIcon
+                icon={faLongArrowDown}
+                className="text-slate-600"
+              />
+            </Button>
+          ) : null}
+        </div>
       </div>
     </div>
   );
