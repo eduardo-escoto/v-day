e616f2778e4f043069cd10e213f427d1bc8cf083
Eduardo Escoto <me@eduardo.wtf>
2022-02-12 00:02:27 -0800

    refactored stuff

--- components/scrollCard.js
+++ components/scrollCard.js
@@ -1,33 +1,37 @@
 import Image from "next/image";
 import randomColor from "randomcolor";
 import Button from "./button";
-import { useRef } from "react";
-export default function ScrollCard(props) {
-  const bg_color = randomColor({ hue: "green", luminosity: "light" });
-  const ref = useRef();
-  const onClickHandler = () => {
-    console.log('test')
-    ref.current.dispatchEvent(
-      new KeyboardEvent("keypress", {
-        key: "PageUp",
-      })
-    );
-  };
+import { forwardRef } from "react";
+export default forwardRef(function ScrollCard(props, ref) {
   return (
     <div
+      style={props.cardStyle}
       ref={ref}
-      style={{ backgroundColor: bg_color }}
       className="p-12 text-3xl flex flex-col text-center justify-center min-h-full"
       id={props.id}
     >
-      <Image src="/test.jpeg" width={400} height={600} />
+      <Image src="/test.jpeg" width={400} height={600} priority={true} />
       <p>{props.text}</p>
       <div>
-        {props.first ? null : (
-          <Button onClick={onClickHandler}>Previous</Button>
-        )}
-        {props.last ? null : <Button>Next</Button>}
+        {props.prevButton ? (
+          <Button
+            onClick={() =>
+              props.prevRef.current.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Previous
+          </Button>
+        ) : null}
+        {props.nextButton ? (
+          <Button
+            onClick={() =>
+              props.nextRef.current.scrollIntoView({ behavior: "smooth" })
+            }
+          >
+            Next
+          </Button>
+        ) : null}
       </div>
     </div>
   );
-}
+});
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import ScrollCard from "../components/scrollCard";
-import Image from "next/image";
-import Button from "../components/button";
-import React, { useRef, createRef, useEffect, forwardRef } from "react";
+import {useMemo, useRef, createRef, useEffect, useState } from "react";
+import randomColor from "randomcolor";
+
 export default function Home() {
-  // const div_ref = useRef();
-  const ind = [0, 1, 2, 3];
-  // const ind_len = ind.length
-  const refs = useRef(ind.map(() => React.createRef()));
-  const onClickH = () => console.log(refs);
+  const ind = useMemo(() => [0, 1, 2, 3], []);
+  const [colors, setColors] = useState([])
+  const refs = useRef(ind.map(() => createRef()));
+  
+  useEffect(() => setColors(ind.map(() => randomColor({luminosity: 'light', hue:'red'}))), [ind])
+
   useEffect(() => {
     refs.current[refs.current.length - 1].current.focus();
   }, [ind]);
@@ -15,72 +16,22 @@ export default function Home() {
   return (
     <div className="flex">
       <div
-        className="basis-full max-h-screen overflow-y-scroll border border-solid border-gray-400"
-        // style={{backgroundColor: {randomColor({ luminosity: "light" })}}}
+        className="basis-full max-h-screen overflow-y-scroll"
       >
-        {ind.map((v, i) => {
-          console.log(i);
-          return (
-            <ScrollCardTest
-              o={onClickH}
-              ref={refs.current[i]}
-              nextRef={i < ind.length - 1 ? refs.current[i + 1] : null}
-              prevRef={i > 0 ? refs.current[i - 1] : null}
-              nextButton={i < ind.length - 1}
-              prevButton={i > 0}
-              key={i}
-              id={i}
-              text={i}
-            />
-          );
-        })}
+        {ind.map((v, i) => (
+          <ScrollCard
+            cardStyle={{backgroundColor: colors[i]}}
+            ref={refs.current[i]}
+            nextRef={i < ind.length - 1 ? refs.current[i + 1] : null}
+            prevRef={i > 0 ? refs.current[i - 1] : null}
+            nextButton={i < ind.length - 1}
+            prevButton={i > 0}
+            key={i}
+            id={i}
+            text={i}
+          />
+        ))}
       </div>
     </div>
   );
 }
-// export default function Home (){
-
-//   console.log(refs)
-
-//   return (<ul>
-//     {['left', 'right'].map((el, i) =>
-//       {
-//         console.log(refs.current)
-//       return <li key={i}><input ref={refs.current[i]} defaultValue={el} /></li>
-//     })}
-//     <button onClick={() => {console.log(refs.current)}}>ef</button>
-//   </ul>)
-// }
-const ScrollCardTest = React.forwardRef(function ScrollCard(props, ref) {
-  return (
-    <div
-      ref={ref}
-      // style={{ backgroundColor: bg_color }}
-      className="p-12 text-3xl flex flex-col text-center justify-center min-h-full"
-      id={props.id}
-    >
-      <Image src="/test.jpeg" width={400} height={600} priority={true} />
-      <p>{props.text}</p>
-      <div>
-        {props.prevButton ? (
-          <Button
-            onClick={() =>
-              props.prevRef.current.scrollIntoView({ behavior: "smooth" })
-            }
-          >
-            Previous
-          </Button>
-        ) : null}
-        {props.nextButton ? (
-          <Button
-            onClick={() =>
-              props.nextRef.current.scrollIntoView({ behavior: "smooth" })
-            }
-          >
-            Next
-          </Button>
-        ) : null}
-      </div>
-    </div>
-  );
-});
